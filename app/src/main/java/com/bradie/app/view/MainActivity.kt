84cd0622b1533a30c3ed815adbaa5bffb353c0bd
Anant Pundir <anantpundir02@gmail.com>
2020-09-18 16:29:41 +0530

    set project structure

--- app/src/main/java/com/bradie/app/view/MainActivity.kt
+++ app/src/main/java/com/bradie/app/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.bradie.app
+package com.bradie.app.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bradie.app.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
