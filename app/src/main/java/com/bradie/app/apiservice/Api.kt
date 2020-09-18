d6d194efffd1dcd3d6baa0358c630adef88b7e96
Anant Pundir <anantpundir02@gmail.com>
2020-09-18 17:43:32 +0530

    add api models

---
+++ app/src/main/java/com/bradie/app/apiservice/Api.kt
@@ -0,0 +1,14 @@
+package com.bradie.app.apiservice
+
+import com.bradie.app.BuildConfig
+import io.reactivex.Flowable
+import retrofit2.http.GET
+import retrofit2.http.Query
+
+interface Api {
+
+    @GET("key=${BuildConfig.ApiKey}")
+    fun getImages(
+        @Query("q") query: String
+    ): Flowable<List<ImagesModel>>
+}
---
+++ app/src/main/java/com/bradie/app/apiservice/ImagesModel.kt
@@ -0,0 +1,18 @@
+package com.bradie.app.apiservice
+
+import com.google.gson.annotations.Expose
+import com.google.gson.annotations.SerializedName
+
+class ImagesModel {
+    @SerializedName("total")
+    @Expose
+    var total: Int? = null
+
+    @SerializedName("totalHits")
+    @Expose
+    var totalHits: Int? = null
+
+    @SerializedName("hits")
+    @Expose
+    var hits: List<Hit>? = null
+}
