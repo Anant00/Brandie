f1a73c9fe35db96bfa1b59afb7569cb85697dca7
Anant Pundir <anantpundir02@gmail.com>
2020-09-18 18:02:59 +0530

    add `AppModule` for dependency injection

---
+++ app/src/main/java/com/bradie/app/di/AppModule.kt
@@ -0,0 +1,46 @@
+package com.bradie.app.di
+
+import com.bradie.app.apiservice.Api
+import com.bradie.app.utils.BASE_URL
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ApplicationComponent
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+import javax.inject.Singleton
+
+@Module
+@InstallIn(ApplicationComponent::class)
+class AppModule {
+
+    @Provides
+    @Singleton
+    fun provideApiService(client: OkHttpClient): Api {
+        val newClient = with(client.newBuilder()) {
+            connectTimeout(1, TimeUnit.MINUTES)
+            readTimeout(1, TimeUnit.MINUTES)
+            writeTimeout(1, TimeUnit.MINUTES)
+        }.build()
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .client(newClient)
+            .addConverterFactory(GsonConverterFactory.create())
+            .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+            .build()
+            .create(Api::class.java)
+    }
+
+    @Singleton
+    @Provides
+    fun provideOkHttpClient(): OkHttpClient {
+        val interceptor = HttpLoggingInterceptor()
+        interceptor.level = HttpLoggingInterceptor.Level.BODY
+        return OkHttpClient.Builder().addInterceptor(interceptor).build()
+    }
+    
+}
