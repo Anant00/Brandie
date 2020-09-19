02010bac6ca983cb15517c76c7398ed8936870e6
Anant Pundir <anantpundir02@gmail.com>
2020-09-19 11:57:48 +0530

    remove extra variables

--- app/src/main/java/com/bradie/app/view/fragments/home/HomeViewModel.kt
+++ app/src/main/java/com/bradie/app/view/fragments/home/HomeViewModel.kt
@@ -2,24 +2,17 @@ package com.bradie.app.view.fragments.home
 
 import androidx.hilt.lifecycle.ViewModelInject
 import androidx.lifecycle.*
-import com.bradie.app.apiservice.ImagesModel
 import com.bradie.app.repository.networkbound.RepoPixabayNetwork
 import com.bradie.app.utils.DEFAULT_QUERY
-import com.bradie.app.utils.ViewStatus
 import kotlinx.coroutines.Dispatchers.IO
 
 class HomeViewModel
 @ViewModelInject
 constructor(private val repoPixabayNetwork: RepoPixabayNetwork): ViewModel() {
 
-    private var _viewStatus: LiveData<ViewStatus<ImagesModel>> = MutableLiveData()
-
     private val _query: MutableLiveData<String> = MutableLiveData("")
     val query: LiveData<String>
         get() = _query
-
-    val viewStatus: LiveData<ViewStatus<ImagesModel>>
-        get() = _viewStatus
 
     fun loadData(query: String) {
         if (query != _query.value) {
