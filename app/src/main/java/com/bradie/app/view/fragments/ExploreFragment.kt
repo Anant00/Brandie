014053b823b8b657565b7193e618c83573b81c63
Anant Pundir <anantpundir02@gmail.com>
2020-09-20 13:09:00 +0530

    add fragments in profile

--- app/src/main/java/com/bradie/app/view/fragments/ExploreFragment.kt
+++ app/src/main/java/com/bradie/app/view/fragments/ExploreFragment.kt
@@ -5,7 +5,12 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.bradie.app.adapters.FOLLOWING_FRAGMENT
+import com.bradie.app.adapters.TRENDING_FRAGMENT
+import com.bradie.app.adapters.ViewPagerAdapter
 import com.bradie.app.databinding.FragmentExploreBinding
+import com.bradie.app.view.fragments.home.FollowingFragment
+import com.google.android.material.tabs.TabLayoutMediator
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -20,5 +25,38 @@ class ExploreFragment: Fragment() {
     ): View? {
         binding = FragmentExploreBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setViewPager()
+    }
+
+
+    private fun setViewPager() {
+        val adapter = object : ViewPagerAdapter(requireActivity())  {
+            override fun createFragment(position: Int): Fragment {
+                return setFragments()[position]?.invoke() ?: throw IndexOutOfBoundsException()
+            }
+        }
+        binding.viewPager.adapter = adapter
+        binding.viewPager.offscreenPageLimit = 3
+
+        TabLayoutMediator(binding.tabs, binding.viewPager) { tab, position ->
+            tab.text = getTabTitle(position)
+        }.attach()
+    }
+
+    private fun getTabTitle(position: Int) = when (position) {
+        TRENDING_FRAGMENT -> "Photos"
+        FOLLOWING_FRAGMENT -> "Brands"
+        else -> ""
+    }
+
+    private fun setFragments(): Map<Int, () -> Fragment> {
+      return  mapOf(
+            TRENDING_FRAGMENT to { FollowingFragment() },
+            FOLLOWING_FRAGMENT to { FollowingFragment() }
+        )
     }
 }
