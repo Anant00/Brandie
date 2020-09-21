31cc7d7c3a3bd57ac72270f20ca5b32ed2593b43
Anant Pundir <anantpundir02@gmail.com>
2020-09-21 20:04:57 +0530

    add docs for class App.kt

--- app/src/main/java/com/bradie/app/App.kt
+++ app/src/main/java/com/bradie/app/App.kt
@@ -3,5 +3,11 @@ package com.bradie.app
 import android.app.Application
 import dagger.hilt.android.HiltAndroidApp
 
+/**
+ * {@annotation HiltAndroidApp} is required by the Dagger-Hilt to inject dependencies
+ * into the Application.
+ *
+ * class App must be set in manifest.
+ */
 @HiltAndroidApp
 class App : Application()
