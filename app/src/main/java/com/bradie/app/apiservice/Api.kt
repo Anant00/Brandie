f972fe68e4d72993e7e977e2a7ca8b009dac8133
Anant Pundir <anantpundir02@gmail.com>
2020-09-23 10:04:59 +0530

    add docs of Api.kt

--- app/src/main/java/com/bradie/app/apiservice/Api.kt
+++ app/src/main/java/com/bradie/app/apiservice/Api.kt
@@ -7,6 +7,12 @@ import retrofit2.http.Query
 
 interface Api {
 
+    /**
+     * @param query is the query that has to be appended to the url.
+     * The api would fetch data pertaining to that query.
+     * @see {https://pixabay.com/api/?key=ApiKeyHere&q=Models}: This is the
+     * format of the url.
+     */
     @GET("?key=${BuildConfig.ApiKey}")
     fun getImages(
         @Query("q") query: String
