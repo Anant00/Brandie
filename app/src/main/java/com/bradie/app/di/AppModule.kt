a8aa3375cf061aecd2902d100ec8853f72bc7e7c
Anant Pundir <anantpundir02@gmail.com>
2020-09-23 10:01:19 +0530

    add docs of AppModule.kt

--- app/src/main/java/com/bradie/app/di/AppModule.kt
+++ app/src/main/java/com/bradie/app/di/AppModule.kt
@@ -15,14 +15,30 @@ import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.Retrofit
 
+/**
+ * This class is used by hilt to inject dependencies.
+ * It should be annotated with @Module so that hilt knows which class
+ * specifies dependencies to be injected.
+ *
+ * Each method must be annotated with @Provides.
+ * IF a method is annotated with @Singleton, this method will have only single instance
+ * of the dependencies throughout the scope.
+ */
 @Module
 @InstallIn(ApplicationComponent::class)
 class AppModule {
 
+    /**
+     * Provide Api class dependency. This would be inject in class REPO: the class
+     * which handles fetching of data from network.
+     */
     @Provides
     @Singleton
     fun provideApiService(client: OkHttpClient): Api {
         val newClient = with(client.newBuilder()) {
+            /**
+             * Setting read and write timeout. Slow internet may cause failure.
+             */
             connectTimeout(1, TimeUnit.MINUTES)
             readTimeout(1, TimeUnit.MINUTES)
             writeTimeout(1, TimeUnit.MINUTES)
@@ -36,6 +52,10 @@ class AppModule {
             .create(Api::class.java)
     }
 
+    /**
+     * OkHttpClient for debugging network response. This client
+     * should be added in Retrofit builder.
+     */
     @Singleton
     @Provides
     fun provideOkHttpClient(): OkHttpClient {
@@ -44,6 +64,9 @@ class AppModule {
         return OkHttpClient.Builder().addInterceptor(interceptor).build()
     }
 
+    /**
+     * This provides dependency for Repo.
+     */
     @Singleton
     @Provides
     fun providePixabayRepo(api: Api): RepoPixabayNetwork {
